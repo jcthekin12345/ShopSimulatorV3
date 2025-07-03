ab3885b4109d0364fe05f5fabac7635f41c3414d
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-03 07:52:00 +0200

    feat: add customer spawn with random budgets

---
+++ spawner.py
@@ -0,0 +1,17 @@
+﻿import random
+
+random.seed()
+
+def spawn_customer():
+    """
+    Generates a random budget for a customer.
+
+    This function simulates the generation of a random budget for a customer by
+    returning a randomly selected integer within a specified range. The budget
+    represents the available funds allocated to the customer.
+
+    :return: An integer representing the budget randomly generated for the customer.
+    :rtype: int
+    """
+    random_budget = random.randrange(1, 100)
+    return random_budget
